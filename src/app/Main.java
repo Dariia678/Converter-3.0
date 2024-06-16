5dacdf74aa0a6cf80c9d8c20c577ed36fcc7813d
Makarenko Dariia <vermicell.i678@gmail.com>
2024-06-16 14:47:04 +0200

    Added functionality for converting Fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,4 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Project Converter: A simple temperature conversion application.");
     }
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5.0/9.0;
+    }
 }
